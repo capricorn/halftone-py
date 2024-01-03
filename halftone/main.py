f7f6bec33975a1c1293e263285d1ebe6daf54295
capricorn <8250820+capricorn@users.noreply.github.com>
2024-01-02 20:30:04 -0700

    Cleanup

--- halftone/main.py
+++ halftone/main.py
@@ -2,13 +2,11 @@ from PIL import Image
 import numpy as np
 
 def apply_kernel(img, f, side=5):
-    # TODO: Better type definition of f
     '''Apply a square kernel f to the image 
 
     f is the sidexside kernel from the image as input and returns a sidexside kernel as output.
     '''
 
-    print(img.shape)
     rpad = side - (img.shape[0] % side)
     cpad = side - (img.shape[1] % side)
 
@@ -20,8 +18,6 @@ def apply_kernel(img, f, side=5):
 
     img = np.pad(img, ((0,rpad), (0,cpad)), constant_values=0)
 
-    print(img.shape)
-
     for row in range(0,rows,side):
         for col in range(0,cols,side):
             arr[row:row+side,col:col+side] = f(img[row:row+side,col:col+side])
@@ -31,21 +27,9 @@ def apply_kernel(img, f, side=5):
 if __name__ == '__main__':
     img = Image.open('lenna.png')
     gimg = img.convert('L')
-
-    # First: given an image, pad it such that it's a multiple of 5.
-
     arr = np.array(gimg)
-    # Pad width and height separately.
-    #arr = np.pad(arr, (arr.shape[0] % 5, arr.shape[1] % 5), constant_values=0)
-    #print(arr.shape)
 
-
-    # Given an image, modify a square slice of it 
-
-    #arr[0:5,0:5] = 0
     def avg_kernel(img: np.ndarray, invert=False):
-        #print(img.shape)
-
         i = 255 if invert else 0
 
         k0 = np.abs(np.array([
@@ -85,26 +69,9 @@ if __name__ == '__main__':
         region = 256/4
         mean = int(img.mean())
 
-        #print(mapping[int(mean/region)])
-
         return mapping[int(mean/region)]
-        '''
-        k = np.ones(img.shape)*mean
-
-        return k
-        '''
-
-        return
 
     arr = apply_kernel(arr, lambda k: avg_kernel(k, invert=False), side=5)
-    print(f'Final shape: {arr.shape}')
-    print(arr)
 
     halftone_img = Image.fromarray(arr).convert('L')
     halftone_img.save('halftone.png')
-    halftone_img.show()
-    '''
-    print(arr.shape[0] % 5)
-    # Here is a 5x5 kernel slice from the image
-    print(arr[0:5,0:5])
-    '''
